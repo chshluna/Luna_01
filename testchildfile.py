be610640bba67ff27813a4fb0283a832cca17a12
chshluna <81735878+chshluna@users.noreply.github.com>
2021-04-01 14:19:06 +0800

    Create testchildfile.py

---
+++ testchildfile.py
@@ -0,0 +1,2 @@
+## Adding a new file in main branch
+print("whatever is in the master file is copied to the child branch. But if a file is created or editted in child branch, it will not reflect in the master branch")
